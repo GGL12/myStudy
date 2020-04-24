0fd1b8a3ef4e547e3990e8bf871cf8f5f399db5c
GGL12 <aka.guo712@gmail.com>
2020-04-24 22:05:42 +0800

    修改目录

--- leetcode/add_two_number.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def add_two_num(self, num1, num2):
-        max_num = 0X7FFFFFFF
-        mask = 0XFFFFFFFF
-        while num2 != 0:
-            num1, num2 = (num1 ^ num2), ((num1 & num2) << 1)
-            num1 = num1 & mask
-            num2 = num2 & mask
-        return num1 if num1 <= max_num else ~(num1 & mask)
--- leetcode/sum_1_n.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def sum_1_n(self, n):
-        return self.sum(n)
-    def sum(self, n):
-        try:
-            1 % n
-            return n + self(n-1)
-        except:
-            return 0
