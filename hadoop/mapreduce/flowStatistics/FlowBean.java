cd84ff893ba56d24b950cd50310127e893079538
GGL12 <aka.guo712@gmail.com>
2020-04-27 19:51:34 +0800

    bean

---
+++ hadoop/mapreduce/flowStatistics/FlowBean.java
@@ -0,0 +1,80 @@
+package mapreduce.flowStatistics;
+
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class FlowBean implements Writable {
+    private long upFlow;
+    private long downFlow;
+    private long sumFlow;
+
+    //使用自定义bean,需要反射调用空参构造方法
+    public FlowBean() {
+        super();
+    }
+
+    //定义toString方法，便于写入数据
+    @Override
+    public String toString() {
+        return upFlow + "\t" + downFlow + '\t' + sumFlow;
+    }
+
+    //定义set方法，将数据存入到bean中
+    public void set(long upFlow, long downFlow) {
+        this.upFlow = upFlow;
+        this.downFlow = downFlow;
+        this.sumFlow = upFlow + downFlow;
+    }
+
+    public long getUpFlow() {
+        return upFlow;
+    }
+
+
+    public void setUpFlow(long upFlow) {
+        this.upFlow = upFlow;
+    }
+
+    public long getDownFlow() {
+        return downFlow;
+    }
+
+    public void setDownFlow(long downFlow) {
+        this.downFlow = downFlow;
+    }
+
+    public long getSumFlow() {
+        return sumFlow;
+    }
+
+    public void setSumFlow(long sumFlow) {
+        this.sumFlow = sumFlow;
+    }
+
+    /**
+     * 序列化方法
+     *
+     * @param out
+     * @throws IOException
+     */
+    public void write(DataOutput out) throws IOException {
+        out.writeLong(upFlow);
+        out.writeLong(downFlow);
+        out.writeLong(sumFlow);
+    }
+
+    /**
+     * 反序列化方法
+     *
+     * @param in
+     * @throws IOException
+     */
+    public void readFields(DataInput in) throws IOException {
+        upFlow = in.readLong();
+        downFlow = in.readLong();
+        sumFlow = in.readLong();
+    }
+}
